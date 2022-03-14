e8ac5e169b53c717fbcb92b8cc1c6045e1bf151b
Adnrew Smal <smalandrey67@ukr.net>
2022-03-15 00:04:55 +0300

    add functional such as return items which had been deleted, reset storage with deleted items

--- script/script.js
+++ script/script.js
@@ -2,27 +2,39 @@
 
 const form = document.querySelector('.form')
 
-//inputs
 const inputAdd = document.querySelector('#input-add')
 const inputSearch = document.querySelector('#input-search') 
 
-//buttons
 const buttonAdd = document.querySelector('#button-add')
 const sortingButtons = document.querySelectorAll('#sorting-button')
+const resetButton = document.querySelector('#reset')
 
 const itemsContainer = document.querySelector('.list')
+const deleteList = document.querySelector('.delete')
 const warning = document.querySelector('.list__warning')
 
 let tasks = []
+let deleteTasks = []
+
 
 function updateLocalStorage(){
     localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function updateDeleteItems(){
+    localStorage.setItem('deleteTasks', JSON.stringify(deleteTasks))
 }
 
 if(localStorage.getItem('tasks')){
     tasks = [...JSON.parse(localStorage.getItem('tasks'))]
 
     createItems(tasks)
+}
+
+if(localStorage.getItem('deleteTasks')){
+    deleteTasks = [...JSON.parse(localStorage.getItem('deleteTasks'))]
+
+    createDeleteItems(deleteTasks)
 }
 
 if(!tasks.length){
@@ -40,7 +52,6 @@ inputAdd.addEventListener('input', (e) => {
 //search tasks
 inputSearch.addEventListener('input', (e) => {
     const visibleItems = tasks.filter(item => item.task.includes(e.target.value.trim().toLowerCase()))
-
 
     createItems(visibleItems)
 })
@@ -66,8 +77,6 @@ function sortingHanlder(e){
    
    createItems(sortingArray)
 }
-
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -119,6 +128,22 @@ function createItems(array){
     inputAdd.value = ''
 }
 
+function createDeleteItems(){
+    deleteList.innerHTML = ''
+    deleteTasks.forEach(task => {
+        deleteList.innerHTML += `
+            <li class="delete__item" data-id="${task.id}">
+                <p class="delete__task ${task.accomplished ? 'list__task--accomplished' : ''} ${task.important ? 'list__task--important' : ''}" >${task.task}</p>
+                <div class="delete__functionality">
+                    <button class="delete__functionality-return button">return</button>
+                </div>
+            </li>
+        `
+    })
+
+    returnFunctionality()
+}
+
 const updateTodo = (filterArray) => {
     tasks = [...filterArray]
 
@@ -126,16 +151,23 @@ const updateTodo = (filterArray) => {
     updateLocalStorage()
 }
 
-
 const deleteHandler = (id) => {
     const filterArray = tasks.filter(item => item.id !== id)
+    const deleteArray = tasks.find(item => item.id === id)
 
+    deleteTasks = [...deleteTasks, deleteArray]
+   
     updateTodo(filterArray)
+    createDeleteItems()
+    updateDeleteItems()
+
     if(tasks.length === 0) {
         warning.classList.add('list__warning--active')
         warning.textContent = 'no tasks yet'
     }
 }
+
+
 
 const accomplishedHandler = (id) => {
     const filterArray = tasks.map(item => item.id === id ? {...item, accomplished: !item.accomplished} : item)
@@ -162,7 +194,7 @@ const changeHandler = (id) => {
         const filterArray = tasks.map(item => {
       
             if(item.id === id){
-                item.task = inputChange.value
+                item.task = inputChange.value.trim().toLowerCase()
             }
             return item
         })
@@ -170,6 +202,27 @@ const changeHandler = (id) => {
         updateTodo(filterArray)
     })
 }
+
+const returnHandler = (id) => {
+    const filterItem = deleteTasks.find(item => item.id === id)
+    const filterDeleteItems = deleteTasks.filter(item => item.id !== id)
+ 
+    deleteTasks = [...filterDeleteItems]
+    tasks = [...tasks, filterItem]
+
+    createItems(tasks)
+    createDeleteItems()
+    updateLocalStorage()
+    updateDeleteItems()
+}
+
+
+resetButton.addEventListener('click', () => {
+    deleteTasks = []
+
+    updateDeleteItems()
+    createDeleteItems()
+})
 
 function addFunctionality(){
     const fieldsContainer = document.querySelectorAll('.list__item')
@@ -187,6 +240,22 @@ function addFunctionality(){
                 importantHandler(fieldId)
             }else if(e.target.classList.contains('list__change')){
                 changeHandler(fieldId)
+            }else return
+
+        })
+    })
+}
+
+function returnFunctionality(){
+    const fieldsContainer = document.querySelectorAll('.delete__item')
+
+    fieldsContainer.forEach(task => {
+        task.addEventListener('click', (e) => {
+            e.preventDefault()
+            const fieldId = +e.target.closest('.delete__item').dataset.id
+
+            if(e.target.classList.contains('delete__functionality-return')){
+                returnHandler(fieldId)
             }else return
         })
     })
