0eee8b0fd6d1d212dd03dc9da31102771eac55f6
Adnrew Smal <smalandrey67@ukr.net>
2022-03-16 10:56:51 +0300

    add style to app

--- script/script.js
+++ script/script.js
@@ -1,30 +1,35 @@
 'use strict'
 
+//main veribles
 const form = document.querySelector('.form')
+const sideBar = document.querySelector('.backet')
+const modalForm = document.querySelector('.modal__content')
+const modal = document.querySelector('#modal')
+const bodyTodo = document.querySelector('.todo__body')
 
+//input veribles
 const inputAdd = document.querySelector('#input-add')
-const inputSearch = document.querySelector('#input-search') 
+const inputSearch = document.querySelector('#input-search')
+const inputChange = document.querySelector('.modal__main-input')
 
+//buttos veribles
 const buttonAdd = document.querySelector('#button-add')
 const sortingButtons = document.querySelectorAll('#sorting-button')
 const resetButton = document.querySelector('#reset')
+const sideBarOpen = document.querySelector('.header__side-image')
+const sideBarCount = document.querySelector('.header__side-count')
+const closeModal = document.querySelector('#close')
 
+//utils veribles
 const itemsContainer = document.querySelector('.list')
-const deleteList = document.querySelector('.delete')
+const deleteList = document.querySelector('.backet__list')
 const warning = document.querySelector('.list__warning')
+const backetWarning = document.querySelector('.backet__warning')
 
 let tasks = []
 let deleteTasks = []
 
-
-function updateLocalStorage(){
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-}
-
-function updateDeleteItems(){
-    localStorage.setItem('deleteTasks', JSON.stringify(deleteTasks))
-}
-
+//==== getting data whenever page loads ====//
 if(localStorage.getItem('tasks')){
     tasks = [...JSON.parse(localStorage.getItem('tasks'))]
 
@@ -34,50 +39,36 @@ if(localStorage.getItem('tasks')){
 if(localStorage.getItem('deleteTasks')){
     deleteTasks = [...JSON.parse(localStorage.getItem('deleteTasks'))]
 
+    createWarningBacket()
     createDeleteItems(deleteTasks)
 }
 
-if(!tasks.length){
-    warning.textContent = 'no tasks yet'
-    warning.classList.add('list__warning--active')
-}else {
-    warning.classList.remove('list__warning--active')
+//==== update local srotage functions ====//
+function updateLocalStorage(){
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-//disable add button if we don't have any letters inside of input
-inputAdd.addEventListener('input', (e) => {
-    e.target.value.trim() ? buttonAdd.classList.remove("disable") : buttonAdd.classList.add("disable");
-})
+function updateDeleteItems(){
+    localStorage.setItem('deleteTasks', JSON.stringify(deleteTasks))
 
-//search tasks
-inputSearch.addEventListener('input', (e) => {
-    const visibleItems = tasks.filter(item => item.task.includes(e.target.value.trim().toLowerCase()))
-
-    createItems(visibleItems)
-})
-
-//sorting functionality
-sortingButtons.forEach(item => {
-    item.addEventListener('click', sortingHanlder)
-}) 
-
-function sortingHanlder(e){
-   const dataType = e.target.dataset.sorting
-   let sortingArray = []
-
-   if(dataType === 'done'){
-       sortingArray = tasks.filter(item => item.accomplished)
-   }else if(dataType === 'important'){
-        sortingArray = tasks.filter(item => item.important)
-   }else if(dataType === 'waite'){
-        sortingArray = tasks.filter(item => !item.accomplished)
-   }else if(dataType === 'all'){
-        sortingArray = tasks
-   }
-   
-   createItems(sortingArray)
+    createWarningBacket()
 }
 
+//==== helper functions ====//
+function createWarningBacket(){
+    sideBarCount.textContent = deleteTasks.length
+}
+
+const updateTodo = (filterArray) => {
+    tasks = [...filterArray]
+
+    createItems(tasks)
+    updateLocalStorage()
+}
+
+
+
+//submit task
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -92,9 +83,91 @@ form.addEventListener('submit', (e) => {
 
     tasks = [...tasks, item]
 
+
     updateLocalStorage()
     createItems(tasks)
 })
+
+//disable (add button) if we don't have any letters inside of input
+inputAdd.addEventListener('input', (e) => {
+    e.target.value.trim() ? buttonAdd.classList.remove("form__button--disable") : buttonAdd.classList.add("form__button--disable");
+})
+
+//listener for searching tasks
+inputSearch.addEventListener('input', (e) => {
+    const visibleItems = tasks.filter(item => item.task.includes(e.target.value.trim().toLowerCase()))
+
+    createItems(visibleItems)
+})
+
+//listener for opening sidebar
+sideBarOpen.addEventListener('click', () => {
+    if(!deleteTasks.length){
+        backetWarning.classList.add('backet__warning--active')    
+    }
+    bodyTodo.classList.toggle('todo__body--active')
+    sideBar.classList.toggle('backet--active')
+})
+
+//listener for closing modal
+closeModal.addEventListener('click', () => modal.classList.remove('modal--active'))
+
+//reset deleted task inside of side bar 
+resetButton.addEventListener('click', () => {
+
+    if(deleteTasks.length){
+        backetWarning.classList.add('backet__warning--active')
+    }
+
+    deleteTasks = []
+
+
+    createWarningBacket()
+    createDeleteItems()
+    localStorage.removeItem('deleteTasks')
+})
+
+
+//==== sorting tasks ====//
+sortingButtons.forEach(item => item.addEventListener('click', sortingHanlder)) 
+
+function sortingHanlder(e){
+    //types of sorting
+    const DONE = 'DONE_TYPE'
+    const IMPORTANT = 'IMPORTANT_TYPE'
+    const NOTDONE = 'NOT_DONE_TYPE'
+    const ALL = 'ALL_TYPE'
+
+    sortingButtons.forEach(item => item.classList.remove('form__sorting-button__active'))
+
+    //add class to current filter type
+    e.target.classList.add('form__sorting-button__active')
+
+    //getting type of sorting from button
+    const dataType = e.target.dataset.sorting
+
+    let sortingArray = []
+
+    if(dataType === DONE) sortingArray = tasks.filter(item => item.accomplished) 
+
+        else if(dataType === IMPORTANT) sortingArray = tasks.filter(item => item.important)  
+
+        else if(dataType === NOTDONE) sortingArray = tasks.filter(item => !item.accomplished)
+        
+        else if(dataType === ALL) sortingArray = tasks
+           
+
+    createItems(sortingArray)
+}
+
+
+if(!tasks.length){
+    warning.textContent = 'no tasks yet'
+    warning.classList.add('list__warning--active')
+}else {
+    warning.classList.remove('list__warning--active')
+}
+
 
 function createItems(array){
     itemsContainer.innerHTML = ""
@@ -113,12 +186,12 @@ function createItems(array){
     array.forEach(task => {
         itemsContainer.innerHTML += `
         <li class="list__item" data-id="${task.id}">
-            <button class="list__change button">change</button> 
-            <p class="list__task ${task.accomplished ? 'list__task--accomplished' : ''} ${task.important ? 'list__task--important' : ''}" >${task.task}</p>
+            <img id="open-modal"  class="list__change" src="images/light-icon.png">
+            <span class="list__task ${task.accomplished ? 'list__task--accomplished' : ''} ${task.important ? 'list__task--important' : ''}" >${task.task}</span>
             <div class="list__functionality">
-                <input class="list__functionality-accomplished input" name="accomplished" ${task.accomplished ? 'checked' : ''} type="checkbox">
-                <button class="list__functionality-delete button">delete</button>
-                <button class="list__functionality-important button">!</button>
+                <input class="list__functionality-accomplished list__functionality-item input" name="accomplished" ${task.accomplished ? 'checked' : ''} type="checkbox">
+                <img class="list__functionality-delete list__functionality-item" src="images/delete-icon.png">
+                <img class="list__functionality-important list__functionality-item" src="images/call-icon.png">
             </div>
         </li>
         `
@@ -128,14 +201,15 @@ function createItems(array){
     inputAdd.value = ''
 }
 
+
 function createDeleteItems(){
     deleteList.innerHTML = ''
     deleteTasks.forEach(task => {
         deleteList.innerHTML += `
-            <li class="delete__item" data-id="${task.id}">
-                <p class="delete__task ${task.accomplished ? 'list__task--accomplished' : ''} ${task.important ? 'list__task--important' : ''}" >${task.task}</p>
-                <div class="delete__functionality">
-                    <button class="delete__functionality-return button">return</button>
+            <li class="backet__item" data-id="${task.id}">
+                <p class="backet__task ${task.accomplished ? 'list__task--accomplished' : ''} ${task.important ? 'list__task--important' : ''}" >${task.task}</p>
+                <div class="backet__functionality">
+                    <button class="backet__functionality-return button">return</button>
                 </div>
             </li>
         `
@@ -144,19 +218,16 @@ function createDeleteItems(){
     returnFunctionality()
 }
 
-const updateTodo = (filterArray) => {
-    tasks = [...filterArray]
-
-    createItems(tasks)
-    updateLocalStorage()
-}
-
 const deleteHandler = (id) => {
     const filterArray = tasks.filter(item => item.id !== id)
     const deleteArray = tasks.find(item => item.id === id)
 
     deleteTasks = [...deleteTasks, deleteArray]
-   
+
+    if(deleteTasks.length){
+        backetWarning.classList.remove('backet__warning--active')  
+    }
+
     updateTodo(filterArray)
     createDeleteItems()
     updateDeleteItems()
@@ -167,8 +238,6 @@ const deleteHandler = (id) => {
     }
 }
 
-
-
 const accomplishedHandler = (id) => {
     const filterArray = tasks.map(item => item.id === id ? {...item, accomplished: !item.accomplished} : item)
 
@@ -176,39 +245,48 @@ const accomplishedHandler = (id) => {
 }
 
 const importantHandler = (id) => {
-    const filterArray = tasks.map(item => item.id === id ? {...item, important: !item.important} : item)
-    
+    const filterArray = tasks.map(item => {
+        if(item.id === id){
+            if(!item.accomplished){
+                return {...item, important: !item.important}
+            }
+        }
+        return item
+    })
+
     updateTodo(filterArray)
 }
 
 const changeHandler = (id) => {
-    const inputChange = document.querySelector('.todo__change')
-    const buttonConfirm = document.querySelector('.todo__confirm')
-    
-    inputChange.classList.add('todo__change--active')
+    modal.classList.add('modal--active')
 
-
-    buttonConfirm.addEventListener('click', () => {
-        // if(!inputChange.value) return
+    modalForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        modal.classList.remove('modal--active')
 
         const filterArray = tasks.map(item => {
-      
             if(item.id === id){
                 item.task = inputChange.value.trim().toLowerCase()
             }
             return item
         })
 
+        inputChange.value = ''
         updateTodo(filterArray)
     })
 }
 
 const returnHandler = (id) => {
     const filterItem = deleteTasks.find(item => item.id === id)
+
     const filterDeleteItems = deleteTasks.filter(item => item.id !== id)
  
     deleteTasks = [...filterDeleteItems]
-    tasks = [...tasks, filterItem]
+    tasks = [filterItem, ...tasks]
+
+    if(!deleteTasks.length){
+        backetWarning.classList.add('backet__warning--active')
+    }
 
     createItems(tasks)
     createDeleteItems()
@@ -216,13 +294,6 @@ const returnHandler = (id) => {
     updateDeleteItems()
 }
 
-
-resetButton.addEventListener('click', () => {
-    deleteTasks = []
-
-    updateDeleteItems()
-    createDeleteItems()
-})
 
 function addFunctionality(){
     const fieldsContainer = document.querySelectorAll('.list__item')
@@ -237,6 +308,7 @@ function addFunctionality(){
             }else if(e.target.classList.contains('list__functionality-accomplished')){
                 accomplishedHandler(fieldId)
             }else if(e.target.classList.contains('list__functionality-important')){
+                e.target.classList.add('.list__functionality-important__active')
                 importantHandler(fieldId)
             }else if(e.target.classList.contains('list__change')){
                 changeHandler(fieldId)
@@ -247,20 +319,20 @@ function addFunctionality(){
 }
 
 function returnFunctionality(){
-    const fieldsContainer = document.querySelectorAll('.delete__item')
+    const fieldsContainer = document.querySelectorAll('.backet__item')
 
     fieldsContainer.forEach(task => {
         task.addEventListener('click', (e) => {
             e.preventDefault()
-            const fieldId = +e.target.closest('.delete__item').dataset.id
+            const fieldId = +e.target.closest('.backet__item').dataset.id
 
-            if(e.target.classList.contains('delete__functionality-return')){
+            if(e.target.classList.contains('backet__functionality-return')){
                 returnHandler(fieldId)
             }else return
+
         })
     })
 }
-
 
 
 
